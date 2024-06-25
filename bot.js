1d9561e153dcf2737e0436330316d21a53198a50
Ser-Ruslan <95072823+Ser-Ruslan@users.noreply.github.com>
2024-06-25 15:23:02 +0900

    quest6

--- bot.js
+++ bot.js
@@ -8,17 +8,38 @@ const bot = new TelegramBot(token, { polling: true });
 
 const fs = require('fs');
 
+const mysql = require('mysql2');
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'ChatBotTests'
+});
+
+db.connect((err) => {
+    if (err) {
+        console.error('Ошибка подключения к базе данных:', err);
+        return;
+    }
+    console.log('Подключено к базе данных');
+});
 
 bot.onText(/\/start/, (msg) => {
     const chatId = msg.chat.id;
     bot.sendMessage(chatId, 'Привет, октагон!');
 });
 
+let userState = {};
+
 bot.setMyCommands([
     { command: '/start', description: 'Приветствие от бота' },
     { command: '/help', description: 'Список команд' },
     { command: '/site', description: 'Ссылка на сайт Октагона' },
-    { command: '/creator', description: 'Автор бота' }
+    { command: '/creator', description: 'Автор бота' },
+    { command: '/randomitem', description: 'Получить случайную строку из бд' },
+    { command: '/deleteitem', description: 'Удалить строку по ID' },
+    { command: '/getitembyid', description: 'Получить строку по ID' }
 ]);
 
 bot.onText(/\/help/, (msg) => {
@@ -29,6 +50,9 @@ bot.onText(/\/help/, (msg) => {
 - /help: Показать этот список команд
 - /site: Получить ссылку на сайт Октагона
 - /creator: Узнать автора бота
+- /randomitem: Получить случайную строку из бд
+- /deleteitem: Удалить строку по ID
+- /getitembyid: Получить строку по ID
 `;
     bot.sendMessage(chatId, helpMessage);
 });
@@ -58,6 +82,87 @@ bot.onText(/\/creator/, (msg) => {
         
         bot.sendMessage(chatId, `Автор бота: ${authorFullName}`);
     });
+});
+
+bot.onText(/\/randomitem/, (msg) => {
+    const chatId = msg.chat.id;
+    const query = 'SELECT * FROM Items ORDER BY RAND() LIMIT 1';
+
+    db.query(query, (err, results) => {
+        if (err) {
+            bot.sendMessage(chatId, 'Произошла ошибка при выполнении запроса.');
+            return console.error(err);
+        }
+
+        if (results.length > 0) {
+            const item = results[0];
+            const response = `(${item.id}) - ${item.name}: ${item.desc}`;
+            bot.sendMessage(chatId, response);
+        } else {
+            bot.sendMessage(chatId, 'Нет доступных предметов.');
+        }
+    });
+});
+
+bot.onText(/\/deleteitem/, (msg) => {
+    const chatId = msg.chat.id;
+    bot.sendMessage(chatId, 'Пожалуйста, введите ID строки, который вы хотите удалить:');
+    userState[chatId] = 'awaiting_delete_id';
+});
+
+bot.onText(/\/getitembyid/, (msg) => {
+    const chatId = msg.chat.id;
+    bot.sendMessage(chatId, 'Пожалуйста, введите ID строки, который вы хотите получить:');
+    userState[chatId] = 'awaiting_get_id';
+});
+
+bot.on('message', (msg) => {
+    const chatId = msg.chat.id;
+    const text = msg.text;
+
+    if (userState[chatId] === 'awaiting_delete_id') {
+        const itemId = parseInt(text, 10);
+        if (isNaN(itemId)) {
+            bot.sendMessage(chatId, 'Пожалуйста, введите правильный ID.');
+        } else {
+            const query = 'DELETE FROM Items WHERE id = ?';
+            db.query(query, [itemId], (err, results) => {
+                if (err) {
+                    bot.sendMessage(chatId, 'Произошла ошибка при выполнении запроса.');
+                    return console.error(err);
+                }
+
+                if (results.affectedRows > 0) {
+                    bot.sendMessage(chatId, 'Удачно');
+                } else {
+                    bot.sendMessage(chatId, 'Ошибка: предмет не найден.');
+                }
+            });
+        }
+        delete userState[chatId];
+    } else if (userState[chatId] === 'awaiting_get_id') {
+        const itemId = parseInt(text, 10);
+        if (isNaN(itemId)) {
+            bot.sendMessage(chatId, 'Пожалуйста, введите правильный ID.');
+        } else {
+            const query = 'SELECT * FROM Items WHERE id = ?';
+            db.query(query, [itemId], (err, results) => {
+                if (err) {
+                    bot.sendMessage(chatId, 'Произошла ошибка при выполнении запроса.');
+                    return console.error(err);
+                }
+
+                if (results.length > 0) {
+                    const item = results[0];
+                    const response = `(${item.id}) - ${item.name}: ${item.desc}`;
+                    bot.sendMessage(chatId, response);
+                } else {
+                    bot.sendMessage(chatId, 'Предмет не найден.');
+                }
+            });
+        }
+        delete userState[chatId];
+    }
 });
 
 module.exports = bot;
