73c9f337451ab7f3ec70678097980bec9d1576bc
Ser-Ruslan <95072823+Ser-Ruslan@users.noreply.github.com>
2024-06-25 14:05:50 +0900

    update

--- app.js
+++ app.js
@@ -1,36 +1,13 @@
-const express = require("express");
-const os = require("os");
-const path = require("path");
-const greeting = require("./greeting");
-
+const express = require('express');
 const app = express();
 const port = 3000;
 
-app.use(express.urlencoded({ extended: true }));  
-app.use(express.static(path.join(__dirname, 'public')));  
 
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+app.get('/', (req, res) => {
+    res.send('<h1>Привет, Октагон!</h1>');
 });
 
-app.get("/api/greeting", (req, res) => {
-    const userName = os.userInfo().username;  
-    res.json({
-        date: greeting.getCurrentDate(),
-        greeting: greeting.getGreetingMessage(userName),
-        userName: userName
-    });
-});
-
-app.post("/", (req, res) => {
-    const userName = req.body.name;  
-    res.json({
-        date: greeting.getCurrentDate(),
-        greeting: greeting.getGreetingMessage(userName),
-        userName: userName
-    });
-});
 
 app.listen(port, () => {
-    console.log(`Сервер начал прослушивание запросов на порту ${port}`);
+    console.log(`Сервер запущен на порту ${port}`);
 });
